1ae14823e7965f67610009fbc5700ec7b92228f8
scoot <scoot@macminiscoot-4.local>
2016-06-05 22:45:29 +0200

    Initial Commit

---
+++ Na4Łapy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Na4Łapy
+//
+//  Created by scoot on 05.06.2016.
+//  Copyright © 2016 Koduj dla Polski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
