810ed2d6a6a29107ba05d04da6b668baf43a7500
Andrzej Butkiewicz <scoot_pl@me.com>
2016-11-04 14:41:26 +0100

    Obsługa płatności

---
+++ Na4Łapy/PomagajViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PomagajViewController.swift
+//  Na4Łapy
+//
+//  Created by scoot on 04.11.2016.
+//  Copyright © 2016 Koduj dla Polski. All rights reserved.
+//
+
+import UIKit
+
+class PomagajViewController: UIViewController {
+
+    @IBAction func pomagaj(_ sender: AnyObject) {
+        let url2 = URL(string: "https://secure.paylane.com/pl.html,Zw7PmynVVL")
+        UIApplication.shared.openURL(url2!)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
